7b93b9d4546706ad6b268c6168b45636c1cc2b5c
Terry-Ding <tianyuding2004@outlook.com>
2025-02-14 15:46:01 -0800

    改对了前十名导演，饼图的意义还要重新思考

--- project/code/data_analysis.py
+++ project/code/data_analysis.py
@@ -1,14 +1,22 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-# fig = plt.figure(figsize=(12, 6), dpi = 80)
 
 def read_file(file_name: str):
     df = pd.read_csv(file_name)
     new_df = df.dropna().drop_duplicates()
     return new_df
 
+def sort_li(li: list[int]) -> list[int]:
+    for i in range(len(li)):
+        for j in range(len(li) - i - 1):
+            if li[j] > li[j + 1]:
+                temp = li[j + 1]
+                li[j + 1] = li[j]
+                li[j] = temp
+    return li
+
 def director_percentage_pie(file_name: str) -> None:
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (12, 6))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (12, 8))
 
     df = read_file(file_name)
     director_name = []
@@ -23,19 +31,35 @@ def director_percentage_pie(file_name: str) -> None:
         else:
             director_dict[key] += 1
 
-    name = []
     count = []
-    for key in director_dict:
-        name.append(key)
-        count.append(director_dict[key])
+    for person in director_dict:
+        count.append(director_dict[person])
 
-    ax1.plot(name, count)
-    ax1.set_xticklabels(name, rotation=45)
-    ax1.set_title("Director Count Plot")
+    sorted_count = sort_li(count)
+    count = sorted_count[-10:]
+    
+    top_ten = []
+    for person in director_dict:
+        if director_dict[person] in count:
+            if len(top_ten) == 10:
+                break
+            top_ten.append(person)
 
-    ax2.pie(count, labels=name)
+    last_names = []
+    for i in top_ten:
+        last_names.append(i.split(" ")[1])
+
+    # bar
+    ax1.bar(top_ten, count)
+    ax1.set_title("directors count")
+    ax1.set_xticks(top_ten, labels = last_names, rotation = 45)
+    ax1.set_yticks(range(min(count), max(count) + 1, 2))
+    
+    # pie
+    ax2.pie(count, labels=last_names)
     ax2.set_title("Director Percentage Pie")
 
     plt.show()
+    
 
 director_percentage_pie("data_analysis_project/project/src/large_data.csv")
